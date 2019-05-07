ac8b16d88c669605ef2b0fb38dccfb960c38cb18
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-05-06 17:04:06 -0700

    [add] Realm Excercise

---
+++ Realm-Starter/Pods/Target Support Files/Pods-Realm-StarterTests/Pods-Realm-StarterTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Realm_StarterTests : NSObject
+@end
+@implementation PodsDummy_Pods_Realm_StarterTests
+@end
---
+++ Realm-Starter/Realm-Starter/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  RealmPractice
+//
+//  Created by Adriana González Martínez on 5/2/19.
+//  Copyright © 2019 Adriana González Martínez. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let store = BookStore()
+        let realm = try! Realm()
+        store.realm = realm
+        
+        //TODO: Create a book
+        //A Game of Thrones (A Song of Ice and Fire #1)
+        //George R. R. Martin"
+        //1997
+        
+        let got = Book(value: ["A Song of Ice and Fire #1", "George R. R. Martin", 1997])
+        //TODO: Save the book
+        
+        store.saveBook(got)
+        //TODO: Find the book by title, print the result.
+        print(store.findBooksByTitle("A Song of Ice and Fire #1"))
+    }
+    
+    
+}
+
